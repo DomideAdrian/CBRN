9b0359d098ba20b8cf3dc88f7a089779432a4df6
Stan Andrei <andreis_2007@yahoo.com>
2018-12-13 18:24:56 +0200

    Updated the models for general use purposes.

--- CBRN_Project/CBRN_Project/MVVM/Models/Input/Icon.cs
+++ CBRN_Project/CBRN_Project/MVVM/Models/Input/Icon.cs
@@ -1,6 +1,44 @@
-﻿namespace CBRN_Project.MVVM.Models
+﻿using System.Collections.Generic;
+
+namespace CBRN_Project.MVVM.Models
 {
     public class Icon
     {
+        #region Fields
+
+        private static uint crrtID = 0;
+
+        #endregion
+
+        #region Properties
+
+        public uint ID          { get; set; }
+        public uint Personnel   { get; set; }
+
+        public BreathingRate BreathingRate  { get; set; }
+        public double BodySurfaceArea       { get; set; }
+        public ProtFactors IPE              { get; set; }
+        public ProtFactors Vehicle_Shelter  { get; set; }
+        public ProtFactors Prophylaxis      { get; set; }
+        public bool Uniform                 { get; set; }
+
+        public List<Challenge> Challenges { get; set; }
+
+        #endregion
+
+        #region Constructors
+
+        public Icon()
+        {
+            ID = crrtID++;
+        }
+
+        #endregion
+
+        #region Methods
+
+
+
+        #endregion
     }
 }
